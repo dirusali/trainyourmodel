80704e3a364893df07d5d23ac4e6328c88e8354e
Diego <dirusali@gmail.com>
2019-07-08 13:35:57 +0200

    Update urls.py

--- mlsaas/urls.py
+++ mlsaas/urls.py
@@ -1,39 +1,45 @@
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.conf.urls import handler404, handler500
+
 
 # TODO : Set correct robots.txt & sitemap
 from django.http import HttpResponse
 from rest_framework_jwt.views import obtain_jwt_token
 from django.views.generic import TemplateView
 from web import settings
+from django.contrib.sitemaps.views import sitemap
+from portal.sitemaps import SearchTagSitemap, CategorySitemap, SearchTagSitemapFemale, StaticViewSitemap
+from portal import views as portal_views
+
+sitemaps = {
+    'tags': SearchTagSitemap,
+    'femaletags': SearchTagSitemapFemale,
+    'categories': CategorySitemap,
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
-
+    url(r'^', include('portal.urls')),    
     url(r'^admin/', admin.site.urls),
     url(r'^google37306863f3b606bc.html$',TemplateView.as_view(template_name='google37306863f3b606bc.html')),
-    url(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nAllow: /", content_type="text/plain")),
+    #url(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nAllow: /", content_type="text/plain")),
     url(r'^api-token-auth/', obtain_jwt_token),
-
     url(r'^scan/', include('scan.urls')),
     url(r'^dashboard/', include('dashboard.urls')),
     url(r'^', include('django_evercookie.urls')),
-    url(r'^', include('portal.urls')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
 # Serve media like categories picture
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+
+handler404 = portal_views.error404
+handler500 = portal_views.error500       
