4377988c1cc2587b1de90f6b438fb81c915eb343
Diego <dirusali@gmail.com>
2019-07-09 01:04:24 +0200

    Update urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -10,16 +10,16 @@ from .views import HomeView, FuncionaView, ContactoView, CondicionesView, Privac
 
 
 urlpatterns = [
-    url(r'^/$', HomeView.as_view(), name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
        # url(r'^/clustering/$', Clustering.as_view(), name='categories'),
         #url(r'^/classification/$', Classification.as_view(), name='categories-detail'),
         #url(r'^/categorical/$', 
         #url(r'^/supervised/$    
-        url(r'^/terms-and-conditions/$', CondicionesView.as_view(), name='terms'),
-        url(r'^/privacy/$', PrivacidadView.as_view(), name='priv'),
-        url(r'^/how-it-works/$', FuncionaView.as_view(), name='funciona'),
-        url(r'^/subscribe/', subscribe, name="subscribe"),
-        url(r'^/contact/$', ContactoView.as_view(), name='contacto'),
-        url(r'^/about/$', RedirectView.as_view(url='/como-funciona/', permanent=True)),
+        url(r'^terms-and-conditions/$', CondicionesView.as_view(), name='terms'),
+        url(r'^privacy/$', PrivacidadView.as_view(), name='priv'),
+        url(r'^how-it-works/$', FuncionaView.as_view(), name='funciona'),
+        url(r'^subscribe/', subscribe, name="subscribe"),
+        url(r'^contact/$', ContactoView.as_view(), name='contacto'),
+        url(r'^about/$', RedirectView.as_view(url='/como-funciona/', permanent=True)),
 ]
 
