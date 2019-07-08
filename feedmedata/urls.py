344fd8980997af158c6bd61752ba06d4c86c81e1
Diego <dirusali@gmail.com>
2019-07-09 00:50:51 +0200

    Update urls.py

--- feedmedata/urls.py
+++ feedmedata/urls.py
@@ -1,4 +1,4 @@
-rom django.conf.urls import url, include
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf.urls import handler404, handler500
