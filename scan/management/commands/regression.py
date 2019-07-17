ab42fec1742afef7118209580ce8bbd892975c5f
Diego <dirusali@gmail.com>
2019-07-17 11:41:38 +0200

    Update regression.py

--- scan/management/commands/regression.py
+++ scan/management/commands/regression.py
@@ -9,7 +9,7 @@ from sklearn import metrics
 boston = load_boston()
 df = pd.DataFrame(boston['data'],columns=boston['feature_names'])
 price = boston['target']
- = df
+X = df
 y = price
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4,random_state=101) 
