47deff93f2771f765de42dc5245b36fa7150174c
Diego <dirusali@gmail.com>
2019-07-25 11:11:23 +0200

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -4,7 +4,10 @@ import operator
 import random
 import unicodedata
 import datetime
-
+mport matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
 from django.template.loader import get_template, render_to_string
 from django.shortcuts import render
 from django.views.generic import TemplateView
@@ -177,31 +180,9 @@ def upload_csv(request):
 		if not csv_file.name.endswith('.csv'):
 			messages.error(request,'File is not CSV type')
 			return HttpResponseRedirect(reverse("portal:upload_csv"))
-        #if file is too large, return
-		if csv_file.multiple_chunks():
-			messages.error(request,"Uploaded file is too big (%.2f MB)." % (csv_file.size/(1000*1000),))
-			return HttpResponseRedirect(reverse("portal:upload_csv"))
 
-		file_data = csv_file.read().decode("utf-8")		
-
-		lines = file_data.split("\n")
-		#loop over the lines and save them in db. If error , store as string and then display
-		for line in lines:						
-			fields = line.split(",")
-			data_dict = {}
-			data_dict["name"] = fields[0]
-			data_dict["start_date_time"] = fields[1]
-			data_dict["end_date_time"] = fields[2]
-			data_dict["notes"] = fields[3]
-			try:
-				form = EventsForm(data_dict)
-				if form.is_valid():
-					form.save()					
-				else:
-					logging.getLogger("error_logger").error(form.errors.as_json())												
-			except Exception as e:
-				logging.getLogger("error_logger").error(repr(e))					
-				pass
+		df = pd.read_csv(csv_file)
+	        return df.head(5)
 
 	except Exception as e:
 	    print(e)
