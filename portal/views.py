c88b2a851827c54809040fee0d51bdbd8a32ef6b
Diego <dirusali@gmail.com>
2019-07-29 12:52:53 +0200

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -229,7 +229,7 @@ def SVM(df,results):
     grid.fit(X_train,y_train)
     grid_predictions = grid.predict(X_test)
     matrix = confusion_matrix(y_test,grid_predictions)
-    report = classification_report(y_test,grid_predictions))
+    report = classification_report(y_test,grid_predictions)
     return (matrix,report) 	
 	
 	
