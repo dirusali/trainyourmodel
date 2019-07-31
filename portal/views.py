469b9178db4135e11494d722296f6ddd8939b271
Diego <dirusali@gmail.com>
2019-07-31 10:49:16 +0200

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -210,7 +210,7 @@ def upload_csv(request):
 			canvas=FigureCanvasAgg(f) 
 			canvas.print_png(buf)
 			response = HttpResponse(buf.getvalue(), content_type='image/png')
-		data = {'results': results, 'graph': canvas}
+		data = {'results': results, 'graph': canvas.print_png(buf)}
 	return render(request, "upload_csv.html", context=data)	
  
 	
