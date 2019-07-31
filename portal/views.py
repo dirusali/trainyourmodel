dc39a5e52cecb02242adea1932f2d02586ce74fa
Diego <dirusali@gmail.com>
2019-07-31 12:01:42 +0200

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -207,7 +207,7 @@ def upload_csv(request):
 			MSE = metrics.mean_squared_error(y_test,pred)
 			MSAE = np.sqrt(metrics.mean_squared_error(y_test,pred))
 			results = "Your model is %s, with MAE: %s MSE: %s. Predictions for your dataset are: %s" % (model, MAE, MSE, pred)	
-			datos = np.array(y_test,pred)
+			datos = (y_test,pred)
 			fig = go.Figure(data=go.Scatter(datos, x="sample", y="predictions", mode='markers', width=800, height=400))
 			graph_div = plotly.offline.plot(fig, auto_open = False, output_type="div")
 			data = {'results': results, 'graph_div': graph_div}
