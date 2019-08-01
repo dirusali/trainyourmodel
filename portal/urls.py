d6e9bd7edf24d5df5f580398e2a55af5c77a328d
Diego <dirusali@gmail.com>
2019-08-01 12:55:21 +0200

    Update urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -17,8 +17,6 @@ urlpatterns = [
         url(r'^contact/$', ContactoView.as_view(), name='contacto'),
         url(r'^about/$', RedirectView.as_view(url='/como-funciona/', permanent=True)),
         url(r'^upload/$', upload_csv, name='upload_csv'),
-        url(r'^scatterplot/$', scatterplot, name='scatterplot'),
-
 
         #urls(r'^results/$, Results.as_view(), name='results')
 ]
