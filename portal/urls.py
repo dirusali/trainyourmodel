294e6cc36f3015a3041480c8a6e9ab6ba8217227
Diego <dirusali@gmail.com>
2019-08-13 20:32:22 +0200

    Update urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.base import RedirectView
 from portal import views as portal_views
 
 app_name = 'portal'
-from .views import HomeView, plottings, upload_csv, FuncionaView, ContactoView, CondicionesView, PrivacidadView, subscribe, successView, contacto
+from .views import HomeView, upload_csv, FuncionaView, ContactoView, CondicionesView, PrivacidadView, subscribe, successView, contacto
 
 
 urlpatterns = [
@@ -14,7 +14,5 @@ urlpatterns = [
         url(r'^subscribe/', subscribe, name="subscribe"),
         url(r'^contact/$', ContactoView.as_view(), name='contacto'),
         url(r'^results/$', upload_csv, name='upload_csv'),
-        url(r'^plots/$', plottings, name='plottings'),
-
 ]
 
