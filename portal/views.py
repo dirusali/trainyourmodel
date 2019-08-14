7a1714447cbc33667270d4d033332ec9368b6d7e
Diego <dirusali@gmail.com>
2019-08-14 14:06:14 +0200

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -278,7 +278,7 @@ def upload_csv(request):
 			    rmse = np.sqrt(metrics.mean_squared_error(y_test,pred))
 			    matrix = confusion_matrix(y_test,pred)	
 			    report = classification_report(y_test,pred)		
-		    context = {'matrix0': matrix[0], 'matrix1': matrix[1], 'mae': mae, 'mse': mse, 'rmse': rmse, 'f1': report[0:52], 'f2': report[54:106], 'f3': report[107:159], 'f4': report[160:213]}           
+		    context = {'matrix0': matrix[0][0], 'matrix01': matrix[0][1], 'matrix10': matrix[1][0], 'matrix11': matrix[1][1], 'mae': mae, 'mse': mse, 'rmse': rmse, 'f1': report[0:52], 'f2': report[54:106], 'f3': report[107:159], 'f4': report[160:213]}           
 		    return render(request, "upload_csv.html", context)	
  
 	
