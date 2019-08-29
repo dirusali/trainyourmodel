cca8968fe0e175f79e3f9b6461ab5a797e82b6c9
Diego <dirusali@gmail.com>
2019-08-29 15:27:03 +0200

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -286,8 +286,7 @@ def upload_csv(request):
 		    if cate == 'apriori':
 			    transactions = df_target
 			    itemsets, rules = apriori(transactions, min_support=0.2,  min_confidence=1
-			    rules_rhs = filter(lambda rule: len(rule.lhs) == 2 and len(rule.rhs) == 1, rules)
-			    rules = sorted(rules_rhs, key=lambda rule: rule.lift)
+			    rules = sorted(rules, key=lambda rule: rule.lift)
 			    context = {'scatter': scatter}           	  
 			    return render(request, "apriori.html", context)			  
 	    if request.POST['submit'] == '_unsuper':
