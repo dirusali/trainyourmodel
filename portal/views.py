ecf1bd9cae71338b0c7044567fc59966f59aba07
Diego <dirusali@gmail.com>
2019-08-29 11:57:43 +0200

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -286,7 +286,7 @@ def upload_csv(request):
 			    Nc = range(1, 20)
 			    kmeans = [KMeans(n_clusters=i) for i in Nc]
 			    score = [kmeans[i].fit(Y).score(Y) for i in range(len(kmeans))]
-		    	score = -1 * score
+			    score = -1 * score
 			    fig = go.Figure(data=go.Scatter(x=y_test, y=pred, mode='markers'))
 			    fig.update_xaxes(title="Number of Clusters")
 			    fig.update_yaxes(title="Error")
