914dcb3e32862a262c7087a44dd8dbbe1fe7e7b3
Diego <dirusali@gmail.com>
2019-09-02 19:05:49 +0200

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -383,11 +383,7 @@ def upload_csv(request):
 	    
 def send_file(request):
 	if request.POST['submit'] == '_download':
-		filename = '/var/www/feedmedata/media/pred.csv'
-		download_name ="predictions.csv"
-		wrapper = FileWrapper(open(filename))
-		response  = HttpResponse(wrapper,content_type='text/csv')
-		response['Content-Disposition'] = "attachment; filename= %s"% download_name
-		context = {'response': response}
-		return render(request, "download_csv.html", context)
+	        response = StreamingHttpResponse(open('/var/www/feedmedata/media/pred.csv'), content_type='text/csv')
+		response['Content-Disposition'] = 'attachment; filename=' + 'predictions.csv'
+		return response
  
