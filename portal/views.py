6b76e196f42b5cd4fa2874ab8cb382475edf4edd
Diego <dirusali@gmail.com>
2019-09-03 13:11:10 +0200

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -302,7 +302,7 @@ def upload_csv(request):
 		        graph_div = plotly.offline.plot(fig, auto_open = False, output_type="div")
 		        context = {'graph_div': graph_div}
 		        return render (request, "plottings.html", context)
-		if grafica == "heatmap":
+	        if grafica == "heatmap":
 		        fig = px.density_heatmap(df_target)
 		        graph_div = plotly.offline.plot(fig, auto_open = False, output_type="div")
 		        context = {'graph_div': graph_div}
