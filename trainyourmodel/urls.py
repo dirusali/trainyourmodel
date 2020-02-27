01ec4bb8d473cfe17813d162166a291b14e52fe3
Diego <dirusali@gmail.com>
2020-02-27 19:30:07 +0100

    Update urls.py

--- trainyourmodel/urls.py
+++ trainyourmodel/urls.py
@@ -7,7 +7,6 @@ from rest_framework_jwt.views import obtain_jwt_token
 from django.views.generic import TemplateView
 from trainyourmodel import settings
 from django.contrib.sitemaps.views import sitemap
-from portal.sitemaps import SearchTagSitemap, CategorySitemap, SearchTagSitemapFemale, StaticViewSitemap
 from portal import views as portal_views
 
 
@@ -15,5 +14,5 @@ from portal import views as portal_views
 urlpatterns = [
     url(r'^', include('portal.urls')),    
     url(r'^admin/', admin.site.urls),
-    #url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-auth/', obtain_jwt_token),
 ]
