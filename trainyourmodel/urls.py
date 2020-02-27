76a2800eafd56d9a9ea08ea078223e1acce1595b
Diego <dirusali@gmail.com>
2020-02-27 19:28:59 +0100

    Update urls.py

--- trainyourmodel/urls.py
+++ trainyourmodel/urls.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf.urls import handler404, handler500
 from django.http import HttpResponse
-#from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token
 from django.views.generic import TemplateView
-from feedmedata import settings
+from trainyourmodel import settings
 from django.contrib.sitemaps.views import sitemap
-#from portal.sitemaps import SearchTagSitemap, CategorySitemap, SearchTagSitemapFemale, StaticViewSitemap
+from portal.sitemaps import SearchTagSitemap, CategorySitemap, SearchTagSitemapFemale, StaticViewSitemap
 from portal import views as portal_views
 
 
