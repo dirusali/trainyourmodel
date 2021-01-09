00273f3004fb16f67548b7ba55cf63e02863f3fd
Diego <dirusali@gmail.com>
2021-01-09 12:44:28 +0100

    Update models.py

--- portal/models.py
+++ portal/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.db.models import Count, CharField, Model
-
+from django.urls import reverse
 
 
 class Subscribe(models.Model):
